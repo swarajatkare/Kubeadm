0c486afa3dffb64c8e8bbb8025a5b3268ebb033e
swarajatkare <161420580+swarajatkare@users.noreply.github.com>
2024-06-22 18:24:13 +0530

    Update master.sh

--- kubeadm/master.sh
+++ kubeadm/master.sh
@@ -5,7 +5,7 @@
 set -e
 
 source /etc/lsb-release
-if [ "$DISTRIB_RELEASE" != "22.04.4 LTS" ]; then
+if [ "$DISTRIB_RELEASE" != "20.04" ]; then
     echo "################################# "
     echo "############ WARNING ############ "
     echo "################################# "
